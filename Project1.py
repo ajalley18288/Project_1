ddc995622d8f01cd5486c8d97f0eda584f71c293
ajalley18288 <alyissasanders@my.unt.edu>
2022-03-04 10:43:37 -0600

    decryption function draft

--- Project1.py
+++ Project1.py
@@ -2,21 +2,63 @@ from Cryptodome.Cipher import AES
 
 starterKey = bytearray(b'\x80\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00')
 
+def cipher_decrypt(ciphertext, key):
+
+    decrypted = ""
+
+    for c in ciphertext:
+
+        if c.islower(): 
+
+            c_index = ord(c) - ord('a') 
+
+            c_og_pos = (c_index - key) % 26 + ord('a')
+
+            c_og = chr(c_og_pos)
+
+            decrypted += c_og
+
+        else:
+
+            decrypted += c
+
+    return decrypted
+
+
+
+
+
 for i in range(16,777,215):
     key1 = starterKey + i.to_bytes(3,'big')
     print(key1)
     # Test if deciphered message equals our plaintext mesage
+    plaintext = input("Enter what file is the plaintext: ")
+    ciphertext = input("Enter what file is the ciphertext: ")
+
+    decrypted_message = cipher_decrypt(ciphertext, i)
+    print("For key {}, decrypted text: {}".format(i, plaintext))
+
     # if they do exit loop
     # if not reset key
 
 for i in range(16,777,215):
     key2 = starterKey + i.to_bytes(3,'big')
     # Test if deciphered message equals our plaintext mesage
+    plaintext = input("Enter what file is the plaintext: ")
+    ciphertext = input("Enter what file is the ciphertext: ")
+
+    decrypted_message = cipher_decrypt(ciphertext, i)
+    print("For key {}, decrypted text: {}".format(i, plaintext))
     # if they do exit loop
     
 for i in range(16,777,215):
     key3 = starterKey + i.to_bytes(3,'big')
     # Test if deciphered message equals our plaintext mesage
+    plaintext = input("Enter what file is the plaintext: ")
+    ciphertext = input("Enter what file is the ciphertext: ")
+
+    decrypted_message = cipher_decrypt(ciphertext, i)
+    print("For key {}, decrypted text: {}".format(i, plaintext))
     # if they do exit loop   
 
 # cipher = AES.new(key, AES.MODE_EAX);
