43bb529eba479aec1f1936d87f9cee820c6de33c
WestfallOwen <100658103+WestfallOwen@users.noreply.github.com>
2022-03-05 15:00:04 -0600

    Fixed the bug in project1test

--- Project1test.py
+++ Project1test.py
@@ -1,28 +1,30 @@
 from Cryptodome.Cipher import AES
+from base64 import b64encode, b64decode
 from utils_demo import *
 
-starterKey = bytearray(b'\x80\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00')
 
-encoding = ["utf8", "cp1525"]
+
+starterKey = bytearray(b'\x80\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00')
+def funnyDecryptTime(ptxt,ctxt,nonce):
+    percent = 0
+    for i in range(16777215):
+        key = starterKey + i.to_bytes(3,'big')
+        pt = decryptor_CTR(ctxt = ctxt, nonce = nonce, key = key)
+        if ptxt == pt:
+            print("I have the golden ticket!")
+            return None
+        if (i%838860) == 0:
+            print(str(percent) + "% done")
+            percent = percent + 5;
+            
  # Read in cipher, plaintext, and nonce 
 for n in range(3):
     plaintext = input("Enter what file is the plaintext: ")
-    f = open(plaintext, "r")
-    plaintextFile = f.read()
-    f.close()
+    plaintextFile = read_file(plaintext)
+    print(plaintextFile)
     ciphertext = input("Enter what file is the ciphertext: ")
-    f = open(ciphertext, encoding = "cp437")
-    ciphertextFile = f.read()
-    f.close()
+    ciphertextFile = read_bytes(ciphertext)
     nonce = input("Enter what file is the nonce: ")
-    f = open(nonce, encoding = "cp437")
-    nonceFile = f.read()
-    f.close()
+    nonceFile = read_bytes(ciphertext)
     funnyDecryptTime(plaintextFile,ciphertextFile,nonceFile)
 
-def funnyDecryptTime(ptxt,ctxt,nonce):
-    for i in range(16,777,215):
-        key1 = starterKey + i.to_bytes(3,'big')
-        print(key1)
-        if ptxt == decryptor_CTR(ctxt, nonce, key1):
-            return None
