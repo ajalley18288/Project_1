87c1238302b254e18d6365b351194c555d61f18f
WestfallOwen <100658103+WestfallOwen@users.noreply.github.com>
2022-03-04 19:12:12 -0600

    An idea commented into the first loop

--- Project1.py
+++ Project1.py
@@ -31,15 +31,15 @@ def cipher_decrypt(ciphertext, key):
 for i in range(16,777,215):
     key1 = starterKey + i.to_bytes(3,'big')
     print(key1)
-    # Test if deciphered message equals our plaintext mesage
+    # Read in cipher, plaintext, and nonce file
     plaintext = input("Enter what file is the plaintext: ")
     ciphertext = input("Enter what file is the ciphertext: ")
-
+     # decrypt using cipher = AES.new(key1, AES.MODE_CTR, nonce)
+     # decrypted_message = cipher.decrypt(ciphertext)
     decrypted_message = cipher_decrypt(ciphertext, i)
     print("For key {}, decrypted text: {}".format(i, plaintext))
-
-    # if they do exit loop
-    # if not reset key
+    # if decrypted_message == m1.txt
+        # quit loop and display key 
 
 for i in range(16,777,215):
     key2 = starterKey + i.to_bytes(3,'big')
