375b6f54e4bcbd6abe5fc235aaba80c991aec98a
WestfallOwen <100658103+WestfallOwen@users.noreply.github.com>
2022-03-06 12:52:13 -0600

    final output changes

--- Project1.py
+++ Project1.py
@@ -6,26 +6,25 @@ starterKey = bytearray(b'\x80\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00')
     
 def funnyDecryptTime(ptxt,ctxt,nonce):
     percent = 0
-    for i in range(16777215):
+    for i in range(16777215): # 2^n -1
         key = starterKey + i.to_bytes(3,'big')
         pt = decryptor_CTR(ctxt = ctxt, nonce = nonce, key = key)
         if string_to_bytes(string_value = ptxt) == pt:
-
-            print("Key: " + key.hex() + " Message: " + pt.decode('utf-8').encode('cp1252').decode('utf-8'))
+            print("Key: " + key.hex())
+            print("Message: " + pt.decode('utf-8').encode('cp1252').decode('utf-8'))
             return None
-        if (i%335544) == 0:
+        if (i%335544) == 0: # .02 * 16,777,215
             print(str(percent) + "% of keys searched")
-            print(key.hex() + " Key currently being run")
+            print("Key currently being run: " + key.hex())
             percent = percent + 2;
             
  # Read in cipher, plaintext, and nonce 
 for n in range(3):
     plaintext = input("Enter what file is the plaintext: ")
     plaintextFile = read_file(fn = plaintext)
-    print(plaintextFile)
+    print(plaintextFile.encode('cp1252').decode('utf-8'))
     ciphertext = input("Enter what file is the ciphertext: ")
     ciphertextFile = read_bytes(fn = ciphertext)
     nonce = input("Enter what file is the nonce: ")
     nonceFile = read_bytes(fn = nonce)
     funnyDecryptTime(plaintextFile,ciphertextFile,nonceFile)
-
